762acd54b0b4a00e45bfa0c5f9f178b88b65d06e
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2021-08-18 16:12:44 +0800

    Added clear() function

--- Chapter08/example4.py
+++ Chapter08/example4.py
@@ -1,14 +1,34 @@
 # ch8/example4.py
 
 import cv2
+import os
 
-im = cv2.imread('input/ship.jpg')
-im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+# define our clear function
+def clear():
+  
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = os.system('clear')
 
-mean_thresh_im = cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-cv2.imwrite('output/mean_thresh_ship.jpg', mean_thresh_im)
+if __name__ == '__main__':
+    clear()
+    cwd = os.getcwd()
+    
+    if os.name == 'nt':
+        im = cv2.imread(os.path.join(cwd, 'Chapter08\input\ship.jpg'))
+    else:
+        im = cv2.imread(os.path.join(cwd, 'Chapter08/input/ship.jpg'))
 
-gauss_thresh_im = cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-cv2.imwrite('output/gauss_thresh_ship.jpg', gauss_thresh_im)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-print('Done.')
+    mean_thresh_im = cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    cv2.imwrite('output/mean_thresh_ship.jpg', mean_thresh_im)
+
+    gauss_thresh_im = cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    cv2.imwrite('output/gauss_thresh_ship.jpg', gauss_thresh_im)
+
+    print('Done.')
