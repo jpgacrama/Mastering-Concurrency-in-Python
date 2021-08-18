500f0fbbef5844829ddfe2d6c36fbdbcae5ee6da
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2021-08-18 11:25:36 +0800

    I cannot see any result for example 3
    That is weird...

--- Chapter08/example2.py
+++ Chapter08/example2.py
@@ -1,16 +1,35 @@
 # ch8/example2.py
 
 import cv2
+import os
 
-im = cv2.imread('input/ship.jpg')
-gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+# define our clear function
+def clear():
+  
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = os.system('clear')
 
-cv2.imshow('Grayscale', gray_im)
-cv2.waitKey(0)
+if __name__ == '__main__':
+    clear()
+    cwd = os.getcwd()
+    
+    if os.name == 'nt':
+        im = cv2.imread(os.path.join(cwd, 'Chapter08\input\ship.jpg'))
+    else:
+        im = cv2.imread(os.path.join(cwd, 'Chapter08/input/ship.jpg'))
 
-print(gray_im)
-print('Type:', type(gray_im))
-print('Shape:', gray_im.shape)
-cv2.imwrite('output/gray_ship.jpg', gray_im)
+    gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Grayscale', gray_im)
+    cv2.waitKey(0)
 
-print('Done.')
+    print(gray_im)
+    print('Type:', type(gray_im))
+    print('Shape:', gray_im.shape)
+    cv2.imwrite('output/gray_ship.jpg', gray_im)
+
+    print('Done.')
--- Chapter08/example3.py
+++ Chapter08/example3.py
@@ -1,11 +1,31 @@
 # ch8/example3.py
 
 import cv2
+import os
 
-im = cv2.imread('input/ship.jpg')
-gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+# define our clear function
+def clear():
+  
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = os.system('clear')
 
-ret, custom_thresh_im = cv2.threshold(gray_im, 127, 255, cv2.THRESH_BINARY)
-cv2.imwrite('output/custom_thresh_ship.jpg', custom_thresh_im)
+if __name__ == '__main__':
+    clear()
+    cwd = os.getcwd()
+    
+    if os.name == 'nt':
+        im = cv2.imread(os.path.join(cwd, 'Chapter08\input\ship.jpg'))
+    else:
+        im = cv2.imread(os.path.join(cwd, 'Chapter08/input/ship.jpg'))
+    
+    gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-print('Done.')
+    ret, custom_thresh_im = cv2.threshold(gray_im, 127, 255, cv2.THRESH_BINARY)
+    cv2.imwrite('output/custom_thresh_ship.jpg', custom_thresh_im)
+
+    print('Done.')
