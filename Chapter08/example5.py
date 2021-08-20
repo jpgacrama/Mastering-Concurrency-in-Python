b49d09182b2724dfcf75705ae65a398f82881cba
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2021-08-20 10:54:45 +0800

    cv2.imread() is now inside process_threshold() but no improvement seen
    Maybe this is the disadvantage of GIL

--- Chapter08/example5.py
+++ Chapter08/example5.py
@@ -9,18 +9,21 @@ sys.path.append(os.getcwd())
 from Chapter08 import *
 
 THRESH_METHOD = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
-INPUT_PATH = 'input/large_input/'
-OUTPUT_PATH = 'output/large_output/'
+
+thismodule = sys.modules[__name__]
+thismodule.INPUT_PATH = 'input/large_input/'
+thismodule.INPUT_PATH = 'output/large_output/'
 
 n = 20
 names = ['ship_%i_%i.jpg' % (i, j) for i in range(n) for j in range(n)]
 
 
-def process_threshold(im, output_name, thresh_method):
+def process_threshold(input_path, output_path, output_name, thresh_method):
+    im = cv2.imread(input_path + output_name)
     gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     thresh_im = cv2.adaptiveThreshold(gray_im, 255, thresh_method, cv2.THRESH_BINARY, 11, 2)
 
-    cv2.imwrite(OUTPUT_PATH + output_name, thresh_im)
+    cv2.imwrite(output_path + output_name, thresh_im)
 
 
 def osFunctions():
@@ -28,10 +31,9 @@ def osFunctions():
     # for windows
     if os.name == 'nt':
         _ = os.system('cls')
-        global INPUT_PATH, OUTPUT_PATH
         cwd = os.getcwd()
-        INPUT_PATH = os.path.join(cwd, 'Chapter08\\input\\large_input\\')
-        OUTPUT_PATH = os.path.join(cwd, 'Chapter08\\output\\large_output\\')
+        thismodule.INPUT_PATH = os.path.join(cwd, 'Chapter08\\input\\large_input\\')
+        thismodule.OUTPUT_PATH = os.path.join(cwd, 'Chapter08\\output\\large_output\\')
   
     # for mac and linux(here, os.name is 'posix')
     else:
@@ -45,7 +47,8 @@ if __name__ == '__main__':
 
         with Pool(n_processes) as p:
             p.starmap(process_threshold, [(
-                cv2.imread(INPUT_PATH + name),
+                thismodule.INPUT_PATH,
+                thismodule.OUTPUT_PATH,
                 name,
                 THRESH_METHOD
             ) for name in names])
